0913de7bb4ae271afdd503873d3f6feaab34ca74
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-07 19:16:08 -0800

    Update task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - print the alphabet in lowercase and then 
  * in uppercase
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int ch;
+	int n;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
-	putchar('\n');
+	for (n = 'a'; n <= 'z'; n++)
+		putnar(n);
+	for (n = 'A'; n <= 'Z'; n++)
+		putnar(n);
+	putnar('\n');
 	return (0);
 }
