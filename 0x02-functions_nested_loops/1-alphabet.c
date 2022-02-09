bc9c2c15313d6e72b936f63ae313325f4ed47092
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-08 20:37:00 -0800

    Update task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
+ *
  * print_alphabet - Prints the alphabet in lowercase
  */
 void print_alphabet(void)
 {
 	char n;
-	n =97;
 	
-	while(n<=122)
+	n = 97;
+	
+	while( n <= 122 )
 	{
 		_putchar(n);
 		n++;
