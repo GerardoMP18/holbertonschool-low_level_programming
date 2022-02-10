5272743101300c70895caa086c8c27371a28a3cf
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-09 21:01:43 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @num1: integer argument num1
+ * @num2: integer argument num2
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
