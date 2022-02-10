a893fae48c10a19d3af9a14154bf0d645e5e1484
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-09 20:54:02 -0800

    Add

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  prints the 9 times table, starting with 0.
+ */
+void jack_bauer(void)
+{
+	int num, mult, prod;
+	for (numero = 0; numero < 10; numero++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult < ; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = numero * mult;
+
+			if (prod < 10 )
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
