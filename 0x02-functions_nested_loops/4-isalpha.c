5a09e62fd0b504832dd8c380cea2debf2b246399
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-09 19:18:03 -0800

    Add task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - for alphabetic character
+ * @c: character 
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
