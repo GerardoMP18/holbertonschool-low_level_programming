4e42fd28d88e8d141e6541723b4e9942edea65b7
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-09 20:12:27 -0800

    Update task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _abs - computes the absolute value of an integer.
+ * _abs - computes the absolute value of an integer.
  * @n: Character to check
  * Return: the absolute value of an integer.
  */
