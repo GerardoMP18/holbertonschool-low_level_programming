dcbd172174830634345f90e164adc6258e63fbb0
GerardoMP <43628789+GerardoMP18@users.noreply.github.com>
2022-02-11 23:33:49 -0500

    Add task

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ * @c:character 
+ * Return: Always (0)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
+}
