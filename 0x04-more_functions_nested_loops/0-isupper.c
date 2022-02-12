48c690dc7803eb441a1f1a59db030b3832908c75
GerardoMP <43628789+GerardoMP18@users.noreply.github.com>
2022-02-11 22:49:35 -0500

    add

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,10 +8,10 @@
   */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        }
 
-	return (0);
+        return (0);
 }
