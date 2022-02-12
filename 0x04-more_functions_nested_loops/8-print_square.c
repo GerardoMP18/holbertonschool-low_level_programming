5271a3d66963caac67d05729127f31e0f7f3ef90
GerardoMP <43628789+GerardoMP18@users.noreply.github.com>
2022-02-12 00:21:43 -0500

    add task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - unction that prints a square, followed by a new line
+ * @size: character to check
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0 ; j < size ; j++)
+		{
+			_putchar('#');
+		}
+		_putchar(10);
+	}
+}
