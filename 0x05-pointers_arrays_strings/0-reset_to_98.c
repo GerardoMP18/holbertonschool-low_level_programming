598e8c223589a13d28d1e797c1814d62514c8627
GerardoMP <gerardomarinparra18@gmail.com>
2022-02-16 03:13:33 +0000

    Update task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*reset_to_98 -  takes a pointer to an int as  *parameter and updates the value it points
+*reset_to_98 -  takes a pointer to an int as  *parameter and updates the value
 *@n: input.
 *Return: no return
 **/
