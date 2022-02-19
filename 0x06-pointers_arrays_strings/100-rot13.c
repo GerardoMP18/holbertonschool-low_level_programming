7911202e4022743f9fcc3c8b351918a8a50f13f1
GerardoMP <43628789+GerardoMP18@users.noreply.github.com>
2022-02-19 00:15:28 -0500

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,7 +15,11 @@ char *rot13(char *s)
 	{
 		for (i = 0; i < 52; i++)
 		{		
-			if (*(s + count) == alphabet[i])																	{																						*(s + count) = rot13[i];																		break;																				}
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
 		}
 		count++;
 	}
