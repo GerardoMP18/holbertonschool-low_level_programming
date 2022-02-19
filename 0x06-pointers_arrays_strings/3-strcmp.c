d1ef6d6bd5e87ac260ab4d507b1195bb1eb8724d
GerardoMP <gerardomarinparra18@gmail.com>
2022-02-19 03:47:54 +0000

    add task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,8 @@ int _strcmp(char *s1, char *s2)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		p = *(s1 + i) - *(s2 + i);							   i++;								
+		p = *(s1 + i) - *(s2 + i);
+		i++;
 	}
 
 	return (p);
