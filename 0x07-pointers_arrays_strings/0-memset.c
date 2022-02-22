3a2f2ad3ce3de1b97e33239417b31f5cc3636520
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-21 22:58:12 -0500

    add task 00

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/*
- * _memset - function that fills memory
- * @s: memory area to filll
- * @b: byte to fill
- * @n: bytes of the memory area pointed
- * Return : pointer to the memory area
- */
+/**
+  * _memset - Function that fills memory
+  * @s: memory area to fill
+  * @b: byte to fill
+  * @n: byte of the memory area pointed
+  *
+  * Return: Pointer to the memory area
+  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
