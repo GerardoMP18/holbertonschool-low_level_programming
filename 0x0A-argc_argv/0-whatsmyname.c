658c35fca507dbdd4781115ab2141562b0809f7c
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-02-25 17:29:30 -0500

    Project argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name in new line
+ * @argc: Arguments count
+ * @argv: Arguments vector
+ * Return: success 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return(0);	
+}	
