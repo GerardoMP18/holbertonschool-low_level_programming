e540525cc2fe374e11cc98d4b25d5f62ff817fe0
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-01 17:26:02 -0500

    Project Malloc and free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Function for creates an arrays of chars
+ * @:size
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+
+	p = malloc(sizeof(*p) * size);
+	
+	if (p == NULL)
+	{
+		return NULL;
+	}
+
+	i = 0;
+	while (i < size)
+	{
+		p[i] = c;
+		i++;
+	}
+
+	return (p);
+}
