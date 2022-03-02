a4b1d8f7da549e0ea085fbbf96b458b094317c96
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-01 22:02:35 -0500

    Update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,11 +31,19 @@ int **alloc_grid(int width, int height)
 		{
 			for (j = i; j >= 0; j--)
 			{
+				free(array[i]);
 				continue;
 			}
 			free(array);
 
 			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
 		}
 	}
 
