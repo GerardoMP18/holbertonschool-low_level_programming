da1469e5f5c5d20973f3c658a5bab1aa089da272
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-01 21:15:50 -0500

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat -Function for concatenates two string
+ * @s1: first charactersi
+ * @s2: second characters
+ * Return: two characters concatenates
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j = 0, k = 0;
+	char *concat;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	i = 0;
+
+	while (s1[i] || s2[i])
+	{
+		k++;
+		i++;
+	}
+
+	concat = malloc(sizeof(char) * k);
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+
+	while (s1[i])
+		concat[j++] = s1[i];
+		i++;
+
+	i = 0;
+
+	while (s2[i])
+		concat[j++] = s2[i];
+		i++;
+
+	return (concat);
+}
