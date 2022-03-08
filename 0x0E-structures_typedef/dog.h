65d45154b474483bd399538e40b6997067ad0853
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-07 22:17:45 -0500

    Exercises 3 solved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - new name for the type strutc dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
