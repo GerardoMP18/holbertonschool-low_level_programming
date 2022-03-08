3308bbd47e19752ccce2ab689855e7cdf71e4cc3
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-07 20:36:43 -0500

    Exercises 1 solved

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - strcuture for dogs
+ * @name: pointer
+ * @age: pointer
+ * @owner: pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
