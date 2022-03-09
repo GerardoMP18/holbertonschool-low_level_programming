bcd95996be2999f31ff9b950ab89d40427dff5b2
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-09 17:05:49 -0500

    Project function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: character
+ * @f: pointer
+ * Return: success
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
