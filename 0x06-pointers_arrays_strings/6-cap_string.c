a56238fb7bb7ef9f0a2465079bec5ec2cb367ade
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-11 01:06:07 -0500

    Correction for betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,9 +19,9 @@ char *cap_string(char *s)
 
 		while (i < cspc)
 		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))														s[a] -= 32;
-
-																								i++;
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+				i++;
 		}
 
 		a++;
