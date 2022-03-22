a7cb105dfce63ee7102037e3f7ca025fa57883e0
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-22 01:52:31 -0500

    adding Project 13

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h: parameter
+ * Return: Number
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
