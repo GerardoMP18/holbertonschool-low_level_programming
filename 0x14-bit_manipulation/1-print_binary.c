5da29cba4e131d0e62ba39ca0d2327f9c0e749f5
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-03-24 20:13:36 -0500

    Adding task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - Function that prints the binary
+ * representation of a number.
+ * @n: number
+ * Return: binary
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
