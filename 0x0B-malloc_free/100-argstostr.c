419198a461d797f96a580e1cffcb4e6e08f21c6c
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-04-03 01:07:04 -0500

    Update

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *argstostr - Entry point
+ *
+ * Description: write a function that concatenates all the arguments
+ * of your program
+ * @ac: arguments count
+ * @av: arguments vector
+ * Return: Always 0 (Success)
+ */
+char *argstostr(int ac, char **av)
+{
+
+	if(ac == 0)
+		return (NULL);
+	if(av == NULL)
+		return (NULL);
+	return(0);
+}
