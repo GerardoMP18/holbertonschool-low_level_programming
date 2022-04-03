df9c1cde0f93da9cc968f086dd9dcb8d5fa25ebe
GerardoMP18 <gerardomarinparra18@gmail.com>
2022-04-03 01:39:07 -0500

    Update palindromo

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _sqrt(int n, int i);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
