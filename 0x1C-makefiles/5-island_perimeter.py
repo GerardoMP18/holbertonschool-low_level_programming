1335621d11c770fe1a7028b2e7b32fc28a94582e
gerardomp18 <gerardomarinparra18@gmail.com>
2022-07-14 17:33:28 -0500

    Correction of pycodestyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,8 @@
 """
 Function that allows counting the perimeter of the island
 """
+
+
 def island_perimeter(grid):
     count_perimeter = 0
     for x in range(len(grid)):
@@ -9,8 +11,7 @@ def island_perimeter(grid):
             if(grid[x][z] == 1):
                 count_perimeter += 4
                 if(grid[x-1][z] == 1):
-                    count_perimeter -=2
+                    count_perimeter -= 2
                 elif(grid[x][z-1] == 1):
                     count_perimeter -= 2
     return count_perimeter
-
