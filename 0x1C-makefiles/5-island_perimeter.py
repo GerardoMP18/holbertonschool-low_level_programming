65d1c691a7b154f0cccadcdac8aa44eec4dac1c0
gerardomp18 <gerardomarinparra18@gmail.com>
2022-07-14 17:28:16 -0500

    Adding exercise to find the perimeter of the island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    count_perimeter = 0
+    for x in range(len(grid)):
+        for z in range(len(grid[x])):
+            if(grid[x][z] == 1):
+                count_perimeter += 4
+                if(grid[x-1][z] == 1):
+                    count_perimeter -=2
+                elif(grid[x][z-1] == 1):
+                    count_perimeter -= 2
+    return count_perimeter
+
