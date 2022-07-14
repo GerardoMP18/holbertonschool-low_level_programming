7929a90223c819bb1fc94ca98950d2e6083be814
gerardomp18 <gerardomarinparra18@gmail.com>
2022-07-14 18:02:48 -0500

    Update task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,11 +11,11 @@ def island_perimeter(grid):
     remove_sides = 0
     perimeter = 0
     for x in range(len(grid)):
-        for z in range(len(grid[x])):
+        for z in range(len(grid[0])):
             if(grid[x][z] == 1):
                 perimeter += 1
                 if(x != 0 and grid[x-1][z] == 1):
                     remove_sides += 1
-                elif(z != 0 and grid[x][z-1] == 1):
+                if(z != 0 and grid[x][z-1] == 1):
                     remove_sides += 1
     return (4 * perimeter) - (2 * remove_sides)
